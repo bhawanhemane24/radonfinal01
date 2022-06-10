cff8ddf31486a5accd3ce1fe60cb575b34ddc857
bhawanhemane24”git config --global user.password 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” <32072889+bhawanhemane24@users.noreply.github.com>
2022-06-10 21:34:11 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const moment = require('moment');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+const  mongoose  = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,17 +12,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+
+//app.use (
+  //  function (req, res, next) {
+    //    console.log ("inside GLOBAL MW");
+      //  next();
+  //}
+ // );
+ app.use(
+    function(req,res,next){
+        console.log("hi ,you see current time,Ip " )
+        let route=req.originalUrl;
+        let userIp=req.socket.remoteAddress
+       let dateTime= moment().format('MMMM Do YYYY, h:mm:ss a')
+       console.log(dateTime)
+        console.log(route);
+        console.log(userIp);
+        next()
+    }
+ )
 
 app.use('/', route);
+
 
 
 app.listen(process.env.PORT || 3000, function () {
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
+ const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 const commonMW = require ("../middlewares/commonMiddlewares")
@@ -12,13 +12,13 @@ router.get("/test-me", function (req, res) {
 
 
 
-router.post("/createBook", BookController.createBook  )
+//router.post("/createBook", BookController.createBook  )
 
 
 
 
 // router.post("/createUser", UserController.createUser  )
-// router.get("/getUsersData", UserController.getUsersData)
+ //router.get("/getUsersData", UserController.getUsersData)
 
 
 // const mid1= function ( req, res, next) {
@@ -48,7 +48,7 @@ router.post("/createBook", BookController.createBook  )
 
 
 
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+//router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
 
 
 
